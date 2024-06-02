c05227d8767b977cbb9997f46632a6343b82e4bd
stalkxk <stalkxk@gmail.com>
2024-06-02 19:10:39 +0300

    cc

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO roles(role_name) VALUES ( 'ROLE_ADMIN' ), ('ROLE_USER');
-
-INSERT INTO users(login, password) VALUES ( 'admin', '$2a$12$u2yg91rk1uw9LtOYXqmaEeIv2mXmraWITBiGaYu2St6L4jDuKr6Qu');
-
-INSERT INTO user_role(user_id, role_id) values ( 1, 1 );
