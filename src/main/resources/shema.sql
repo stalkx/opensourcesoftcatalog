1d7dbe4b11ecccdf7a07874109adec6535d5e0af
stalkxk <stalkxk@gmail.com>
2024-06-02 18:56:47 +0300

    c

--- src/main/resources/shema.sql
+++ src/main/resources/shema.sql
@@ -43,10 +43,3 @@ CREATE TABLE user_role (
                            role_id INT REFERENCES roles(role_id) ON DELETE CASCADE,
                            PRIMARY KEY (user_id, role_id)
 );
-
-
-INSERT INTO roles(role_name) VALUES ( 'ROLE_ADMIN' ), ('ROLE_USER');
-
-INSERT INTO users(login, password) VALUES ( 'admin', '$2a$12$u2yg91rk1uw9LtOYXqmaEeIv2mXmraWITBiGaYu2St6L4jDuKr6Qu');
-
-INSERT INTO user_role(user_id, role_id) values ( 1, 1 );
